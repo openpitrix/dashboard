ad9beba0f624e7122a165d12477b58cb614a3196
LBR <bernieli@yunify.com>
2018-01-03 20:41:19 +0800

    import bootstrap

---
+++ src/containers/InstalledApps/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './index.scss';
+
+export default class InstalledApps extends Component {
+  render() {
+    return (
+      <div className="apps">
+        Nav
+      </div>
+    );
+  }
+}
