40c5312bb8ee2949a725154ec0149c7a0b79a042
LBR <bernieli@yunify.com>
2018-01-09 10:55:51 +0800

    add manage-apps page

--- server/routes/apps.js
+++ server/routes/apps.js
@@ -1,5 +1,6 @@
 import Apps from '../mock/apps.json';
 import App from '../mock/app.json';
+import InstalledApps from '../mock/installed-apps.json';
 
 export async function getApps(ctx) {
   const apps = await Promise.resolve(Apps);
@@ -16,4 +17,16 @@ export async function getApps(ctx) {
 export async function getApp(ctx) {
   const app = await Promise.resolve(App);
   ctx.body = app;
+}
+
+export async function getInstalledApps(ctx) {
+  const apps = await Promise.resolve(InstalledApps);
+
+  ctx.body = {
+    total_items: 2,
+    total_pages: 1,
+    page_size: 10,
+    current_page: 1,
+    items: apps,
+  };
 }
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -4,6 +4,7 @@ import request from 'core/request';
 export default class AppStore {
   @observable apps = [];
   @observable app = {};
+  @observable installedApps = [];
   @observable isLoading = false;
 
   @action async fetchApps() {
@@ -17,6 +18,13 @@ export default class AppStore {
     this.isLoading = true;
     const result = await request.get(`api/app/${appId}`);
     this.app = result;
+    this.isLoading = false;
+  }
+
+  @action async fetchInstalledApps() {
+    this.isLoading = true;
+    const result = await request.get('api/installedApps');
+    this.installedApps = result.items;
     this.isLoading = false;
   }
 }
