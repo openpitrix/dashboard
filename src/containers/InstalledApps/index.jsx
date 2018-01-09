5b0bd1b0bf1a02dfc32418706261a5d2b609000d
LBR <bernieli@yunify.com>
2018-01-09 10:55:51 +0800

    add manage-apps page

--- src/containers/InstalledApps/index.jsx
+++ src/containers/InstalledApps/index.jsx
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 
+import { Link } from 'react-router-dom';
 import ManageTabs from 'components/ManageTabs';
 import Button from 'components/Base/Button';
 import Input from 'components/Base/Input';
+import Select from 'components/Base/Select';
 import styles from './index.scss';
 
 @inject('rootStore')
 @observer
 export default class InstalledApps extends Component {
+  static async onEnter({ appStore }) {
+    await appStore.fetchInstalledApps();
+  }
+
   render() {
+    const { appStore } = this.props.rootStore;
+
     return (
       <div className={styles.apps}>
         <ManageTabs />
 
         <div className={styles.container}>
           <div className={styles.toolbar}>
-            <Button className={styles.dropdown}>All types</Button>
+            <Select className={styles.select} value="All Types">
+              <Select.Option value="1">Types1</Select.Option>
+              <Select.Option value="2">Types2</Select.Option>
+            </Select>
             <Input.Search className={styles.search} placeholder="Search App Name"/>
           </div>
 
           <ul className={styles.list}>
-            <li className={styles.listItem}>
-              <img className={styles.icon} src="http://via.placeholder.com/96x96" />
-              <div className={styles.name}><a href="#">HarshData</a></div>
-              <div className={styles.handle}>
-                <Button type="primary">Deploy</Button>
-                <Button>...</Button>
-              </div>
-            </li>
+            {
+              appStore.installedApps && appStore.installedApps.map(item =>
+                <li key={item.id} className={styles.listItem}>
+                  <img className={styles.icon} src={item.icon || 'http://via.placeholder.com/96x96'}/>
+                  <div className={styles.name}>
+                    <Link to={`/app/${item.id}`}>{item.name}</Link>
+                  </div>
+                  <div className={styles.handle}>
+                    <Link to={`/app/${item.id}/deploy`}>
+                      <Button type="primary">Deploy</Button>
+                    </Link>
+                    <Button>...</Button>
+                  </div>
+                </li>,
+              )
+            }
           </ul>
         </div>
       </div>
