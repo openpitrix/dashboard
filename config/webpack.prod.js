a5a283904675abb51a75218f1a1e2921e4f45750
libangrui <librf2e@gmail.com>
2018-01-18 16:23:20 +0800

    modify the merge of the webpack-config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra-promise');
+const mergeWith = require('lodash/mergeWith');
 const path = require('path');
 const webpack = require('webpack');
 const config = require('./webpack.base.js');
@@ -7,7 +8,13 @@ const postCssOptions = require('./postcss.options');
 
 const root = (dir) => path.resolve(__dirname, '..', dir);
 
-Object.assign(config, {
+console.info('Clearing Build Path');
+
+fs.emptyDirSync(root('build'));
+
+console.info('Environment: Production');
+
+mergeWith(config, {
   cache: false,
   entry: {
     bundle: root('src/index.js'),
@@ -16,72 +23,70 @@ Object.assign(config, {
     path: root('build'),
     publicPath: '/build/',
   },
-});
-
-console.info('Clearing Build Path');
-
-fs.emptyDirSync(root('build'));
-
-console.info('Environment: Production');
-
-config.module.loaders.push({
-  test: /\.s[ac]ss$/,
-  loader: ExtractCSS.extract({
-    fallback: 'style-loader',
-    use: [
+  module: {
+    loaders: [
       {
-        loader: 'css-loader',
-        options: {
-          minimize: true,
-          importLoaders: 1,
-          localIdentName: '[folder]__[local]___[hash:base64:5]',
-          modules: true,
-        },
+        test: /\.s[ac]ss$/,
+        loader: ExtractCSS.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                importLoaders: 1,
+                localIdentName: '[folder]__[local]___[hash:base64:5]',
+                modules: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: postCssOptions,
+            },
+            { loader: 'sass-loader' },
+          ],
+        }),
+        include: [root('src')],
       },
       {
-        loader: 'postcss-loader',
-        options: postCssOptions,
-      },
-      { loader: 'sass-loader' },
-    ],
-  }),
-  include: [root('src')],
-});
-
-config.module.loaders.push({
-  test: /\.css$/,
-  loader: ExtractCSS.extract({
-    fallback: 'style-loader',
-    use: [
-      {
-        loader: 'css-loader',
-        options: {
-          minimize: true,
-          importLoaders: 1,
-        },
+        test: /\.css$/,
+        loader: ExtractCSS.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                importLoaders: 1,
+              },
+            },
+          ],
+        }),
       },
     ],
-  }),
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new webpack.DefinePlugin({
+      'process.env.DEV': false,
+      'process.env.BROWSER': true,
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
+}, (objValue, srcValue) => {
+  if (Array.isArray(objValue)) {
+    return objValue.concat(srcValue);
+  }
 });
-
-config.plugins.push(
-  new webpack.optimize.OccurrenceOrderPlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      screw_ie8: true,
-      warnings: false,
-    },
-    output: {
-      comments: false,
-    },
-  }),
-);
-
-config.plugins.push(new webpack.DefinePlugin({
-  'process.env.DEV': false,
-  'process.env.BROWSER': true,
-  'process.env.NODE_ENV': JSON.stringify('production'),
-}));
 
 const compiler = webpack(config);
 compiler.run((err, stats) => {
