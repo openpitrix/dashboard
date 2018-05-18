931acf29570db017caf0782de39fbfbe1d744d4a
iwisunny <iwisunny@gmail.com>
2018-05-18 16:08:11 +0800

    cherry-pick:82f305f, fix webpack.dev publicPath, add missing test snapshot

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: resolve(__dirname, 'build/'),
-    publicPath: '/',
+    publicPath: '/build',
     pathinfo: true
   },
   profile: true,
