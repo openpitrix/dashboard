224883e5b2a8c298ee3b08a587de306f61963e89
wangxi <iwisunny@gmail.com>
2018-06-05 13:29:23 +0800

    upgrade jest and react-popper, fix testing unhandled promise exception (#83)

--- tests/setup.js
+++ tests/setup.js
@@ -8,3 +8,23 @@ global.requestAnimationFrame =
   function(cb) {
     return setTimeout(cb, 0);
   };
+
+/*
+ fix jest unhandle promise
+@see https://github.com/facebook/jest/issues/3251
+ */
+process.on('unhandledRejection', reason => {
+  console.log('REJECTION', reason);
+});
+
+/*
+ fix testing react-popper with jsdom
+@see: https://github.com/FezVrasta/popper.js/issues/478
+*/
+global.document.createRange = () => ({
+  setStart: () => {},
+  setEnd: () => {},
+  commonAncestorContainer: {
+    nodeName: 'BODY'
+  }
+});
