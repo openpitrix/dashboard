6d0b7b80521d63eac8594d9ec51973dfd6b988dd
wangxi <iwisunny@gmail.com>
2018-06-07 11:55:24 +0800

    fix: css module tag missing in production build (#86)
    - fix login route api msg handle
    - koa-static add max-age http header to use browser caching

--- src/stores/LoginStore.js
+++ src/stores/LoginStore.js
@@ -1,16 +1,16 @@
-import { action, observable } from 'mobx';
+import { action, toJS } from 'mobx';
 import request from 'lib/request';
 import Store from './Store';
 
 export default class LoginStore extends Store {
-  @observable errMsg = '';
-
   @action
   async login(params) {
-    try {
-      await request.post('login', params);
-    } catch (err) {
-      this.errMsg = err;
+    let apiMsg = await request.post('login', params);
+
+    apiMsg = toJS(apiMsg);
+
+    if (!apiMsg.success) {
+      this.notifyMsg = toJS(apiMsg).msg;
     }
   }
 }
