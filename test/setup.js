b54e04ad13b8cfa8232033e6372c6f54c722c76c
wangxi <iwisunny@gmail.com>
2018-11-12 17:21:49 +0800

    fix: CodeMirror mock in test

--- test/setup.js
+++ test/setup.js
@@ -33,7 +33,7 @@ global.document.createRange = () => ({
 
 global.window.scroll = () => {};
 
-jest.mock('CodeMirror', () => <div />);
+jest.mock('react-codemirror', () => <div />);
 jest.mock('react-i18next', () => ({
   // this mock makes sure any components using the translate HoC receive the t function as a prop
   translate: () => Component => {
