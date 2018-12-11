9be0719aa6c8dbf827e4ac054bda0c136547cffa
dongrui <dannydong@yunify.com>
2018-12-11 15:48:49 +0800

    feat: Add new app versions manage page
    * feat: Add new version detail page

---
+++ src/config/version-types.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    icon: 'vm-icon',
+    name: 'VM',
+    value: 'vmbased',
+    intro: 'delivery_type_intro_vm'
+  },
+  {
+    icon: 'helm-icon',
+    name: 'Helm',
+    value: 'helm',
+    intro: 'delivery_type_intro_helm'
+  },
+  {
+    icon: 'saas-icon',
+    name: 'SaaS',
+    value: 'saas',
+    intro: 'delivery_type_intro_saas'
+  },
+  {
+    icon: 'api-icon',
+    name: 'API',
+    value: 'api',
+    intro: 'delivery_type_intro_api'
+  },
+  {
+    icon: 'native-icon',
+    name: 'Native',
+    value: 'native',
+    intro: 'delivery_type_intro_native'
+  },
+  {
+    icon: 'serveless-icon',
+    name: 'Serveless',
+    value: 'serveless',
+    intro: 'delivery_type_intro_serveless'
+  }
+];
--- src/pages/Dashboard/Apps/Versions/index.jsx
+++ src/pages/Dashboard/Apps/Versions/index.jsx
@@ -3,12 +3,13 @@ import { observer, inject } from 'mobx-react';
 import { Link } from 'react-router-dom';
 import { translate } from 'react-i18next';
 import classnames from 'classnames';
+import _ from 'lodash';
 
 import { Icon, Button } from 'components/Base';
-import Layout, {
-  Grid, Row, Section, Card
-} from 'components/Layout';
+import Layout from 'components/Layout';
+import Status from 'components/Status';
 
+import versionTypes from 'config/version-types';
 import styles from './index.scss';
 
 @translate()
@@ -23,7 +24,7 @@ export default class Versions extends Component {
     const { appVersionStore, match } = this.props;
     const { appId } = match.params;
 
-    await appVersionStore.fetchAll({ app_id: appId });
+    await appVersionStore.fetchAppVersions(appId);
   }
 
   componentWillUnmount() {
@@ -31,98 +32,154 @@ export default class Versions extends Component {
     appVersionStore.reset();
   }
 
+  toggleHistoryVersions(typeVersion) {
+    typeVersion.isShow = !typeVersion.isShow;
+  }
+
+  renderTypes(types) {
+    const { t } = this.props;
+    // get not added types
+    const notAddedTypes = versionTypes.filter(
+      item => !types.includes(item.value)
+    );
+
+    return (
+      <div>
+        {notAddedTypes.map(item => (
+          <div key={item.value} className={styles.notAddedType}>
+            <Icon
+              name={item.icon}
+              size={48}
+              className={styles.icon}
+              type="dark"
+            />
+            <div className={styles.name}>{t(item.name)}</div>
+            <div className={styles.description}>{t(item.intro)}</div>
+            <Button className={styles.button} type="primary">
+              <Icon name="add" type="white" className={styles.addIcon} />
+              {t('New version')}
+            </Button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  renderHistoryVersions(typeVersion) {
+    const { t } = this.props;
+    const versions = typeVersion.versions;
+    const historyVersions = versions.filter(
+      item => item.status === 'suspended'
+    );
+
+    if (historyVersions.length === 0) {
+      return <div className={styles.total}>没有历史版本</div>;
+    }
+
+    return (
+      <Fragment>
+        <div className={styles.total}>
+          以及 {historyVersions.length} 个历史版本
+          <span
+            onClick={() => this.toggleHistoryVersions(typeVersion)}
+            className={styles.toggleIcon}
+          >
+            {t(typeVersion.isShow ? 'Collapse' : 'Expand')}
+            <Icon
+              name={typeVersion.isShow ? 'minus-square' : 'plus-square'}
+              size={16}
+              className={styles.icon}
+            />
+          </span>
+        </div>
+        {typeVersion.isShow && (
+          <ul className={styles.historyVersion}>
+            {historyVersions.map(item => (
+              <li key={item.version_id}>
+                <Link to={`/dashboard/version/${item.version_id}`}>
+                  <Status
+                    type={item.status}
+                    name={item.name}
+                    className={styles.status}
+                  />
+                  <label className={styles.description}>
+                    {item.description}
+                  </label>
+                  <label className={styles.time}>
+                    {t('下架时间')}: {item.update_time}
+                  </label>
+                  <label className={styles.link}>{t('View detail')}→</label>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </Fragment>
+    );
+  }
+
+  renderActiveVersions(typeVersion) {
+    const { t } = this.props;
+    const versions = typeVersion.versions;
+    const activeVersions = versions.filter(item => item.status !== 'suspended');
+
+    return (
+      <Fragment>
+        <div className={styles.total}>
+          当前有 {activeVersions.length} 个活跃版本
+        </div>
+        <div className={styles.activeVersion}>
+          {activeVersions.map(item => (
+            <Link
+              key={item.version_id}
+              className={classnames(styles.version, [styles[item.status]])}
+              to={`/dashboard/version/${item.version_id}`}
+            >
+              <Status
+                type={item.status}
+                name={item.name}
+                className={styles.status}
+              />
+              {t(_.capitalize(item.status))}
+            </Link>
+          ))}
+        </div>
+        {this.renderHistoryVersions(typeVersion)}
+      </Fragment>
+    );
+  }
+
   render() {
     const { appVersionStore, t } = this.props;
     const { versions } = appVersionStore;
-    const addMethods = [
-      {
-        iconName: 'cloud',
-        name: 'VM',
-        versions
-        /* versions: [
-          { name: '0.0.1', status: 'passed' },
-          { name: '0.0.2', status: 'suspended' },
-          { name: '0.0.3', status: 'active' },
-          { name: '0.0.4', status: 'rejected' },
-          { name: '0.0.5', status: 'submitted' },
-          { name: '0.0.6', status: 'draft' }
-        ] */
-      },
-      {
-        iconName: 'bigdata',
-        name: '业务',
-        versions: [{ name: '0.0.1', status: 'draft' }, { name: '0.0.2', status: 'submitted' }]
-      }
-    ];
-    const otherMethods = [
-      {
-        iconName: 'container',
-        name: 'Helm',
-        description:
-          '轻量级、可移植、自包含的软件打包技术，使应用可以在几乎任何地方以相同的方式运行。'
-      },
-      {
-        iconName: 'vnc',
-        name: '原生',
-        description:
-          '轻量级、可移植、自包含的软件打包技术，使应用可以在几乎任何地方以相同的方式运行。'
-      },
-      {
-        iconName: 'laptop',
-        name: 'SaaS',
-        description:
-          '云端集中式托管软件及其相关的数据，软件仅需透过互联网，而不须透过安装即可使用。'
-      },
-      {
-        iconName: 'pod',
-        name: 'Serverless',
-        description: 'Serverless Framework是无服务器应用框架和生态系统。'
-      }
-    ];
+    const types = versions.map(item => item.type);
 
     return (
-      <Layout className={styles.versions} detailPage="版本">
-        <div>
-          <div className={styles.title}>已添加的交付方式(2)</div>
-          {addMethods.map(method => (
-            <div key={method.name} className={styles.addMethod}>
-              <div className={styles.name}>
-                <Icon name={method.iconName} size={24} className={styles.icon} type="dark" />
-                {method.name}
-              </div>
-              <div className={styles.versions}>
-                <span className={styles.total}>版本：{method.versions.length}</span>
-                {method.versions.map(version => (
-                  <Link
-                    key={version.name}
-                    className={classnames(styles.version, [styles[version.status]])}
-                    to={version.version_id ? `/dashboard/version/${version.version_id}` : '#'}
-                  >
-                    <label className={styles.dot} />
-                    {version.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          ))}
+      <Layout className={styles.versions} pageTitle="版本管理" isCenterPage>
+        <div className={styles.noteWords}>
+          每个应用可支持多种交付方式，每种交付方式的版本是相互独立的。
         </div>
 
-        <div>
-          <div className={styles.title}>其他交付方式</div>
-          {otherMethods.map(method => (
-            <div key={method.name} className={styles.otherMethod}>
-              <div className={styles.name}>
-                <Icon name={method.iconName} size={20} className={styles.icon} type="dark" />
-                {method.name}
-              </div>
-              <div className={styles.description}>{method.description}</div>
-              <Button className={styles.button} type="primary">
-                <Icon name="add" type="white" className={styles.addIcon} />
-                创建新版本
+        <div className={styles.typeTitle}>{t('已添加')}</div>
+        {versions.map(item => (
+          <div key={item.type} className={styles.addedVersion}>
+            <div className={styles.title}>
+              {(_.find(versionTypes, { value: item.type }) || {}).name}
+              <Button className={styles.button} type="default">
+                <Icon name="add" type="dark" className={styles.addIcon} />
+                {t('New version')}
               </Button>
             </div>
-          ))}
-        </div>
+            {this.renderActiveVersions(item)}
+          </div>
+        ))}
+
+        {types.length < 6 && (
+          <Fragment>
+            <div className={styles.typeTitle}>{t('未添加')}</div>
+            {this.renderTypes(types)}
+          </Fragment>
+        )}
       </Layout>
     );
   }
