f86420675adc573994778a0f385e39155f46a79c
lizhi <liiil825.2012@gmail.com>
2019-01-11 11:10:36 +0800

    test: Cover Upload onChange

--- test/components/Base/Upload.test.js
+++ test/components/Base/Upload.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 import Upload from 'components/Base/Upload';
+
+const findByRef = wrapper => ref => wrapper.findWhere(node => node.getDOMNode() === wrapper.instance()[ref]);
+
+const fileName = 'test.zip';
 
 describe('Base/Upload', () => {
   it('basic render', () => {
@@ -10,8 +14,18 @@ describe('Base/Upload', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
-  it('basic render', () => {
-    const wrapper = render(<Upload />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+  it('call onChange', () => {
+    const wrapper = mount(<Upload />);
+
+    wrapper.findByRef = findByRef(wrapper);
+
+    const input = wrapper.findByRef('fileInput');
+    input.simulate('change', { target: { files: [fileName] } });
+  });
+
+  it('disabled', () => {
+    const wrapper = mount(<Upload disabled />);
+    expect(wrapper.prop('disabled')).toBeTruthy();
+    wrapper.unmount();
   });
 });
