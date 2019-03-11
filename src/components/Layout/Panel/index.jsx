c396961aad0aad8f7e406ccce6806d5594b029a6
lizhi <liiil825.2012@gmail.com>
2019-03-11 21:08:07 +0800

    style: Auto set the DetailTabs padding top to 0

--- src/components/Layout/Panel/index.jsx
+++ src/components/Layout/Panel/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import _ from 'lodash';
 
 import styles from './index.scss';
 
+const TAB_NAME = 'Translate(DetailTabs)';
+
 const Panel = ({ className, children, ...rest }) => (
-  <div className={classnames(styles.panel, className)} {...rest}>
+  <div
+    className={classnames(
+      styles.panel,
+      {
+        [styles.paddingTop0]:
+          _.get(children, '[0].type.displayName') === TAB_NAME
+      },
+      className
+    )}
+    {...rest}
+  >
     {children}
   </div>
 );
