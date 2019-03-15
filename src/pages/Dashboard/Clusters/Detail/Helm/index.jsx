f160fb04d8e459b606f66c0a55220e92b3664527
lizhi <liiil825.2012@gmail.com>
2019-03-15 10:06:42 +0800

    fix: Search i18n

--- src/pages/Dashboard/Clusters/Detail/Helm/index.jsx
+++ src/pages/Dashboard/Clusters/Detail/Helm/index.jsx
@@ -64,7 +64,7 @@ export default class HelmCluster extends React.Component {
     return (
       <Toolbar
         className={styles.toolbar}
-        placeholder={t('Search Pods')}
+        placeholder={t(`Search ${nodeType} Pods`)}
         searchWord={searchNode}
         onSearch={onSearchNode}
         onClear={onClearNode}
